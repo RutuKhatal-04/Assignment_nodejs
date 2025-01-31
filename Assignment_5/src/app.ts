876f8870fb170b2c73f3e29bd67cf58aac29a5e7
RutuKhatal-04 <rutukhatal13@gmail.com>
2025-01-31 12:34:12 +0530

    Initial commit with all assignments

---
+++ Assignment_5/src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from 'body-parser';
+import authroutes from "./routes/authroutes";
+import { authenticateJWT } from "./middleware/auth";
+import dotenv from 'dotenv';
+dotenv.config();
+// import authRoutes from './routes/authroutes';
+// import { authenticateJWT, authenticateJWTcust } from './middleware/authMiddleware';
+// import "./models/associations";
+// import cron from "node-cron";
+
+
+const app=express();
+const PORT=process.env.PORT;
+app.use(express.json());
+app.use(bodyParser.json());
+app.use('/auth',authroutes);
+app.use('/home',authenticateJWT("Admin"),authroutes);
+app.use('/employee',authenticateJWT("Employee"),authroutes);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
